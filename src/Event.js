5b1f745283d9fe214c122e56113e96181dcfd865
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-05-20 18:10:52 -0500

    Adds and tests event data

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+    state = {
+        showDetails: false
+    };
+    toggleDetails = (theState) => {
+        this.setState({
+            showDetails: !theState
+        });
+    };
+
     render() {
-        return <div></div>;
+        const { event } = this.props;
+        const { showDetails } = this.state;
+        return (
+            <div className="event">
+                <h2 className="eventTitle">{event.summary}</h2>
+                <p className="eventData"><span className="eventTime">{new Date(event.start.dateTime).toString()}</span><br />
+                    &#64; {event.summary} &#124; <span className="eventPlace">{event.location}</span></p>
+                {showDetails && (
+                    <div class="eventDetails">
+                        <h3>About Event</h3>
+                        <a className="eventLink" href="{event.htmlLink}">See Details on Google Calendar</a>
+                        <p className="eventDescription">{event.description}</p>
+                    </div>
+                )}
+                <button className="toggleDetails" onClick={() => this.toggleDetails(showDetails)}>{showDetails ? "hide" : "show"} details</button>
+            </div>
+        );
     }
 }
 export default Event;
