581aa2ab2debaa79c49678c4f8ce1438f7bf8063
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-06-09 02:29:12 -0500

    verifies authorization for usage

--- src/api.js
+++ src/api.js
@@ -1,5 +1,116 @@
+<<<<<<< Updated upstream
+=======
+import { mockData } from './mock-data';
+import axios from 'axios';
+import NProgress from 'nprogress';
+
+>>>>>>> Stashed changes
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
     var locations = [...new Set(extractLocations)];
     return locations;
+<<<<<<< Updated upstream
 };
+=======
+};
+
+const checkToken = async (accessToken) => {
+    const result = await fetch(
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+        .then((res) => res.json())
+        .catch((error) => error.json());
+
+    return result;
+};
+
+// const getToken = async (code) => {
+//     const encodeCode = encodeURIComponent(code);
+//     const { access_token } = await fetch(
+//         'https://oauth2.googleapis.com/token' + '/' + encodeCode
+//     )
+//         .then((res) => {
+//             return res.json();
+//         })
+//         .catch((error) => error);
+
+//     access_token && localStorage.setItem("access_token", access_token);
+
+//     return access_token;
+// };
+
+const getToken = async (code) => {
+    try {
+        const encodeCode = encodeURIComponent(code);
+
+        const response = await fetch('https://oauth2.googleapis.com/token' + '/' + encodeCode);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const { access_token } = await response.json();
+        access_token && localStorage.setItem("access_token", access_token);
+        return access_token;
+    } catch (error) {
+        error.json();
+    }
+}
+
+const removeQuery = () => {
+    if (window.history.pushState && window.location.pathname) {
+        var newurl =
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname;
+        window.history.pushState("", "", newurl);
+    } else {
+        newurl = window.location.protocol + "//" + window.location.host;
+        window.history.pushState("", "", newurl);
+    }
+};
+
+export const getEvents = async () => {
+    NProgress.start();
+
+    if (window.location.href.startsWith("http://localhost")) {
+        NProgress.done();
+        return mockData;
+    }
+
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = 'YOUR_GET_EVENTS_API_ENDPOINT' + '/' + token;
+        const result = await axios.get(url);
+        if (result.data) {
+            var locations = extractLocations(result.data.events);
+            localStorage.setItem("lastEvents", JSON.stringify(result.data));
+            localStorage.setItem("locations", JSON.stringify(locations));
+        }
+        NProgress.done();
+        return result.data.events;
+    }
+};
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        if (!code) {
+            const results = await axios.get(
+                "https://wy78jb5a1m.execute-api.us-east-2.amazonaws.com/dev"
+            );
+            const { authUrl } = results.data;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
+}
+>>>>>>> Stashed changes
