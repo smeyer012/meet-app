c0eca27dc6ee32e22aaff2bf8f3ad4186f48859a
Ian White <89417609+ian-zen@users.noreply.github.com>
2023-08-09 12:52:40 -0500

    breaking!: upgrade react and react-dom to latest versions. clean up code

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,6 @@
 import '@testing-library/jest-dom';
 
 import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 Enzyme.configure({ adapter: new Adapter() });
