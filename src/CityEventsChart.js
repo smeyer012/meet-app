2c531e52a6dd68f7873bbfeaed4796d6e00e712a
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2023-08-11 10:56:18 -0500

    simplifies how data state is set in CityEventsChart

--- src/CityEventsChart.js
+++ src/CityEventsChart.js
@@ -13,17 +13,13 @@ const CityEventsChart = ({ allLocations, events }) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        setData(getData());
-    }, [`${data}`]);
-
-    const getData = () => {
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length
-            const city = location.split(', ')[0]
+            const city = location.split((/, | - /))[0]
             return { city, count };
         })
-        return data;
-    };
+        setData(data);
+    }, [allLocations, events]);
 
     return (
         <ResponsiveContainer width="99%" height={400} >
@@ -31,13 +27,13 @@ const CityEventsChart = ({ allLocations, events }) => {
                 margin={{
                     top: 20,
                     right: 20,
-                    bottom: 20,
-                    left: 20,
+                    bottom: 60,
+                    left: -30,
                 }}
             >
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
-                <YAxis type="number" dataKey="count" name="Number of Events" />
+                <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
+                <YAxis type="number" dataKey="count" name="Number of Events" allowDecimals={false} />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name="Events per city" data={data} fill="#8884d8" />
             </ScatterChart>
